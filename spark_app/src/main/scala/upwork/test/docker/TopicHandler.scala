31ea4a88530c31021aa616e3b0dc132c04f337ac
Dominik Lenda <dominik.lenda@protonmail.com>
2023-11-02 16:11:17 +0100

    prepare Spark app for development and testing inside of the container

--- spark_app/src/main/scala/upwork/test/docker/TopicHandler.scala
+++ spark_app/src/main/scala/upwork/test/docker/TopicHandler.scala
@@ -6,7 +6,8 @@ import org.apache.spark.sql.cassandra.DataStreamWriterWrapper
 object TopicHandler {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Handle data from Kafka topic").getOrCreate()
-    spark.conf.set("spark.cassandra.connection.host", "127.0.0.1")
+    val cassandraConnHost = args(0)
+    spark.conf.set("spark.cassandra.connection.host", cassandraConnHost)
     spark.conf.set("spark.cassandra.auth.username", "cassandra")
     spark.conf.set("spark.cassandra.auth.password", "cassandra")
     spark.conf.set("spark.sql.catalog.mycatalog", "com.datastax.spark.connector.datasource.CassandraCatalog")
@@ -14,25 +15,25 @@ object TopicHandler {
     spark.sql("CREATE DATABASE IF NOT EXISTS mycatalog.testks WITH DBPROPERTIES (class='SimpleStrategy',replication_factor='1')")
     spark.sql("CREATE TABLE IF NOT EXISTS mycatalog.testks.user (key STRING, value STRING) USING cassandra PARTITIONED BY (key)")
 
-//    //List their contents
-//    spark.sql("SHOW NAMESPACES FROM mycatalog").show()
-//    spark.sql("SHOW TABLES FROM mycatalog.testks").show()
+   //List their contents
+   spark.sql("SHOW NAMESPACES FROM mycatalog").show()
+   spark.sql("SHOW TABLES FROM mycatalog.testks").show()
 
 
     import spark.implicits._
-    val host = args(0)
-    val port = args(1)
+    val kafkaHost = args(1)
+    val kafkaPort = args(2)
     val df = spark
       .readStream
       .format("kafka")
       .option("startingOffsets", "latest")
-      .option("kafka.bootstrap.servers", s"$host:$port")
+      .option("kafka.bootstrap.servers", s"$kafkaHost:$kafkaPort")
       .option("subscribe", "registration")
       .load()
     val rawDF = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
 
-    val dfc = spark.read.table("mycatalog.testks.user")
-    dfc.show()
+    // val dfc = spark.read.table("mycatalog.testks.user")
+    // dfc.show()
 
 //    val query = rawDF.writeStream
 //      .outputMode("append")
