8a5bde49752a7d7b9aed566416096327ffafcadd
Dominik Lenda <dominik.lenda@protonmail.com>
2023-11-02 22:00:46 +0100

    generate OTP and imitate email sending by just writing it to a file

--- spark_app/src/main/scala/upwork/test/docker/TopicHandler.scala
+++ spark_app/src/main/scala/upwork/test/docker/TopicHandler.scala
@@ -1,6 +1,10 @@
 package upwork.test.docker
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.cassandra.DataStreamWriterWrapper
+import com.bastiaanjansen.otp.{HMACAlgorithm, HOTPGenerator, SecretGenerator}
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
 
 object TopicHandler {
   def main(args: Array[String]): Unit = {
@@ -12,8 +16,8 @@ object TopicHandler {
     spark.conf.set("spark.sql.catalog.mycatalog", "com.datastax.spark.connector.datasource.CassandraCatalog")
     spark.conf.set("spark.sql.catalog.mycatalog", "com.datastax.spark.connector.datasource.CassandraCatalog")
 
-     spark.sql("CREATE DATABASE IF NOT EXISTS mycatalog.testks WITH DBPROPERTIES (class='SimpleStrategy',replication_factor='1')")
-     spark.sql("CREATE TABLE IF NOT EXISTS mycatalog.testks.user (key STRING, value STRING) USING cassandra PARTITIONED BY (key)")
+    spark.sql("CREATE DATABASE IF NOT EXISTS mycatalog.testks WITH DBPROPERTIES (class='SimpleStrategy',replication_factor='1')")
+    spark.sql("CREATE TABLE IF NOT EXISTS mycatalog.testks.user (key STRING, value STRING) USING cassandra PARTITIONED BY (key)")
 
     import spark.implicits._
     val kafkaHost = args(1)
@@ -28,23 +32,47 @@ object TopicHandler {
       .load()
     val rawDF = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
 
-    val dfc = spark.read.table("mycatalog.testks.user")
-    dfc.show()
-  // val query = rawDF.writeStream
-  //   .outputMode("append")
-  //   .format("console")
-  //   .start()
-  // query.awaitTermination()
+//    val dfc = spark.read.table("mycatalog.testks.user")
+//    dfc.show()
+
+     val query = rawDF.writeStream
+       .outputMode("append")
+       .format("console")
+       .start()
 
 //    val checkpointLocation = "/tmp/check_point"
-//   rawDF.writeStream
-//     .option("checkpointLocation", checkpointLocation)
-//     .format("org.apache.spark.sql.cassandra")
-//     .cassandraFormat("user", "testks")
-//     .outputMode("append")
-//     .start().awaitTermination()
+//    val query = rawDF.writeStream
+//      .option("checkpointLocation", checkpointLocation)
+//      .format("org.apache.spark.sql.cassandra")
+//      .cassandraFormat("user", "testks")
+//      .outputMode("append")
+//      .start()
 
+    val otp = createOTP(8)
+    val emailAdress = "dlenda1729@gmail.com"
+    mockEmailSending(emailAdress, otp)
+
+    query.awaitTermination()
 
   }
+
+  def createOTP(passwordLength: Int): String = {
+    val secret = SecretGenerator.generate()
+    val hotp = new HOTPGenerator.Builder(secret)
+      .withPasswordLength(8)
+      .withAlgorithm(HMACAlgorithm.SHA256)
+      .build()
+    val counter = 5
+    hotp.generate(counter)
+  }
+
+  def mockEmailSending(emailAddress: String, otp: String): Unit = {
+    val content = s"OTP for $emailAddress is $otp"
+    val path = Paths.get("emailOTP.txt")
+    Files.write(path, content.getBytes(StandardCharsets.UTF_8))
+  }
+
+
+
 
 }
