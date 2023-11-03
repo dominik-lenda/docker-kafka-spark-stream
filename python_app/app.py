ae84cd69004b9f7e9f7284f731b781e50b543314
Dominik Lenda <dominik.lenda@protonmail.com>
2023-11-03 12:48:22 +0100

    produce to new otp topic

--- python_app/app.py
+++ python_app/app.py
@@ -7,15 +7,15 @@ from confluent_kafka.admin import AdminClient, NewTopic
 import uuid
 
 # create topic so that spark can see it when a topic does not exist (kafka volume is mounted for the first time)
-# admin_client = AdminClient({
-#     "bootstrap.servers": "kafka0:29092"
-# })
-# email_topic = NewTopic("email", num_partitions=1, replication_factor=1)
-# otp_topic = NewTopic("otp", num_partitions=1, replication_factor=1)
-# admin_client.create_topics([email_topic])
+admin_client = AdminClient({
+    "bootstrap.servers": "kafka0:29092"
+})
+email_topic = NewTopic("email", num_partitions=1, replication_factor=1)
+otp_topic = NewTopic("otp", num_partitions=1, replication_factor=1)
+admin_client.create_topics([email_topic])
 
-# conf = {'bootstrap.servers': 'kafka0:29092'}
-# producer = Producer(conf)
+conf = {'bootstrap.servers': 'kafka0:29092'}
+producer = Producer(conf)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key12381'
@@ -28,14 +28,16 @@ class OtpForm(FlaskForm):
     otp = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+session = {}
 @app.route('/', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        id = str(uuid.uuid4())
+        session['id'] = str(uuid.uuid4())
         email = request.form['email']
-        # producer.produce("email_topic", key=id, value=email)
-        # producer.flush()
+        producer.produce("email_topic", key=id, value=email)
+        producer.flush()
+
         return redirect(url_for('otp'))
     return render_template('register.html', form=form)
 
@@ -43,10 +45,11 @@ def register():
 def otp():
     form = OtpForm()
     if form.validate_on_submit():
-        id = str(uuid.uuid4())
+        id = session['id']
         otp = request.form['otp']
-        # producer.produce("otp_topic", key=id, value=otp)
-        # producer.flush()
+        producer.produce("otp_topic", key=id, value=otp)
+        producer.flush()
+
         if otp == '123':
             return redirect(url_for('home'))
         else:
